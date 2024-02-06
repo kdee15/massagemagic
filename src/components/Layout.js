4b8fbc14f0c0dd6199c9ef1ab68bcbb602165261
intembeko <kirkdaniels@me.com>
2024-02-06 16:53:35 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
 
-export default function Layout({ children, title, description }) {
+export default function Layout({ children }) {
+  const title =
+    "Mobile Massage Therapy in Cape Town - Massages, Manicures & Pedicures";
+  const description =
+    "Indulge in the luxury of relaxation with our mobile massage therapy services. Our expert therapists bring the spa experience to your doorstep, offering rejuvenating massages in the comfort of your own space.";
   return (
     <>
       <Head>
