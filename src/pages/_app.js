996ac376ee4e13d5903b92a1e26e1f619cc9c98f
intembeko <kirkdaniels@me.com>
2024-02-21 21:23:30 +0200

    whatsapp & tel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,16 +7,16 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <GoogleTagManager gtmId="GTM-5PRSHNZ4" />
+      <a href="tel:0826631066" className={`aCallMe`}>
+        <span>0826631066</span>
+      </a>
+
       <Component {...pageProps} />
-      <script
-        src="https://static.elfsight.com/platform/platform.js"
-        data-use-service-core
-        defer
-      ></script>
-      <div
-        class="elfsight-app-ce1b623c-6244-4677-b0ed-e54f72aaa150"
-        data-elfsight-app-lazy
-      ></div>
+      <a
+        href="https://wa.me/+270826631066"
+        target="_blank"
+        className={`aWhatsappMe`}
+      ></a>
     </Layout>
   );
 }
